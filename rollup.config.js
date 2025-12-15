042555b896893fc3de91728dfdc8921c6cc73203
neon-x-hub <abdomem7@gmail.com>
2025-12-15 12:52:28 +0100

    Chore: configure rollup build for library output

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      exports: 'auto' // Better CommonJS detection
+    },
+    {
+      file: 'dist/esm/index.js',
+      format: 'es'
+    }
+  ],
+};
