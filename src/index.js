8052f1621b34cd75d2413b3bee770670c271a745
neon-x-hub <abdomem7@gmail.com>
2025-12-15 12:52:28 +0100

    Integration: expose core Ledger through package entry point

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import Clock from './core/Clock';
+import Layer from './core/Layer';
+import CommitNode from './core/CommitNode';
+import Ledgex from './core/Ledger';
+
+export { Clock, Layer, CommitNode, Ledgex };
